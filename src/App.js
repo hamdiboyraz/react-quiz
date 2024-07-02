30924d552b4af2964b50755fa20c81e0464c899a
hamdiboyraz <hamdiboyraz19@gmail.com>
2024-07-03 00:28:35 +0300

    Add show answer and update points

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ const initialState = {
     questions: [],
     status: "loading", // "loading", "error", "ready", "active", "finished"
     index: 0, // currentQuestion
-    answers: []
+    answer: null,
+    points: 0,
 }
 
 function reducer(state, action) {
@@ -31,13 +32,20 @@ function reducer(state, action) {
                 ...state,
                 status: "active"
             }
+        case "newAnswer":
+            const currentQuestion = state.questions[state.index]; // state.questions.at(state.index)
+            return {
+                ...state,
+                answer: action.payload,
+                points: action.payload === currentQuestion.correctOption ? state.points + currentQuestion.points : state.points
+            }
         default:
             throw new Error("Unknown action");
     }
 }
 
 function App() {
-    const [{questions, status, index}, dispatch] = useReducer(reducer, initialState);
+    const [{questions, status, index, answer}, dispatch] = useReducer(reducer, initialState);
 
     const numberOfQuestions = questions.length;
 
@@ -55,7 +63,7 @@ function App() {
                 {status === "loading" && <Loader/>}
                 {status === "error" && <Error/>}
                 {status === "ready" && <StartScreen numberOfQuestions={numberOfQuestions} dispatch={dispatch}/>}
-                {status === "active" && <Question question={questions[index]}/>}
+                {status === "active" && <Question question={questions[index]} answer={answer} dispatch={dispatch}/>}
             </Main>
         </div>
     );
